7e29f4bdf0dcd3c4bce06a6024a2421c9254c2da
Sugin Paladin <34408293+sugin223pl@users.noreply.github.com>
2024-03-10 00:15:33 +0200

    Add files

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,29 +1,117 @@
-import { app, shell, BrowserWindow, ipcMain, dialog } from 'electron'
-import { join, basename, extname } from 'node:path';
-import { readFile } from 'node:fs/promises';
-import { readFileSync, writeFileSync } from 'node:fs';
-//import handwritten from 'handwritten.js';
-import Jimp from 'jimp';
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset';
-import mime from 'mime-types';
-import piexif from 'piexifjs';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('fix-esm').register()
+const { app, shell, BrowserWindow, ipcMain, dialog } = require('electron')
+const { join, basename, extname } = require('node:path')
+// const { initialize, trackEvent } = require('@aptabase/electron/main')
+const { readFileSync, writeFileSync } = require('node:fs')
+const { electronApp, optimizer, is } = require('@electron-toolkit/utils')
+const { machineIdSync } = require('node-machine-id')
+const debug = require('electron-debug')
+const Jimp = require('jimp')
+const piexif = require('piexifjs')
+const { contentType } = require('mime-types')
+const unhandled = require('electron-unhandled')
+const fetch = require('node-fetch')
+const MainStore = require('electron-store')
 
-function get_random (list) {
-  return list[Math.floor((Math.random()*list.length))];
+const machineID = machineIdSync({ original: true })
+const schema = {
+  LICENSE_KEY: {
+    type: 'string',
+    default: ''
+  },
+  LICENSE_ACTIVE: {
+    type: 'boolean',
+    default: false
+  },
+  MACHINE_NAME: {
+    type: 'string',
+    default: machineID
+  },
+  INSTANCE_ID: {
+    type: 'string',
+    default: ''
+  },
+  ACTIVATION_STATUS: {
+    type: 'boolean',
+    default: false
+  }
 }
+const mainStore = new MainStore({ schema })
+
+unhandled()
+debug({
+  isEnabled: false
+})
+// initialize('A-EU-4206936490')
+const headers = {
+  'content-type': 'application/json',
+  accept: 'application/json'
+}
+
+async function LemonActivation(key, action = 'validate') {
+  const postRequest = await fetch(`https://api.lemonsqueezy.com/v1/licenses/${action}`, {
+    method: 'POST',
+    headers,
+    body:
+      mainStore.get('INSTANCE_ID') == ''
+        ? JSON.stringify({
+            license_key: key,
+            instance_name: action === 'validate' ? null : machineID
+          })
+        : JSON.stringify({
+            license_key: key,
+            instance_id: mainStore.get('INSTANCE_ID')
+          })
+  })
+
+  const postData = await postRequest.json()
+
+  if (action === 'deactivate') {
+    mainStore.clear()
+  }
+  if (mainStore.get('INSTANCE_ID') != '') {
+    if (!postData.valid) {
+      mainStore.clear()
+    }
+  }
+  // console.log('--------------------' + action + ' Machine---------------')
+  // console.log('Machine ID', machineID)
+  // console.log(postData)
+  // console.log('-----------------------------------------------------')
+
+  if ('instance' in postData) {
+    if (postData.instance != null && 'id' in postData.instance) {
+      mainStore.set('INSTANCE_ID', postData.instance.id)
+      if (action === 'activate') {
+        mainStore.set('ACTIVATION_STATUS', postData.activated)
+        //console.log('SET ACTIVATION_STATUS::: ', mainStore.get('ACTIVATION_STATUS'))
+      } else {
+        // console.log('CHECK ACTIVATION_STATUS:: ', mainStore.get('ACTIVATION_STATUS'))
+      }
+    }
+  }
+  return {
+    valid: action === 'validate' ? postData.valid : postData.activated,
+    error: postData.error
+  }
+}
+// 2B2F39E2-B58E-4C19-B564-F9FCA962FCCD
+
+function get_random(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+const fontPath = join(__dirname, '../../jeURtOG_ZKxNX4XOyeLeaTzn.ttf.fnt')
 const exifs = [
-  'exifs/iphone-12-pro.jpg',
-  'exifs/iphone-x.jpg',
-  'exifs/iphone-xs-max.jpg',
-  'exifs/motorola-one-hyper.jpg',
-  'exifs/nikon.jpg',
-  'exifs/panasonic.jpg',
-  'exifs/sony.jpg',
-];
-
-
-
+  join(__dirname, '../../iphone-12-pro.jpg'),
+  join(__dirname, '../../iphone-x.jpg'),
+  join(__dirname, '../../iphone-xs-max.jpg'),
+  join(__dirname, '../../motorola-one-hyper.jpg'),
+  join(__dirname, '../../nikon.jpg'),
+  join(__dirname, '../../panasonic.jpg'),
+  join(__dirname, '../../sony.jpg')
+]
 
 const Store = {
   image: '',
@@ -34,8 +122,10 @@ const Store = {
   exifin: '',
   raw: ''
 }
-const getBase64DataFromJpegFile = filename => readFileSync(filename).toString('binary');
-const getExifFromJpegFile = filename => piexif.load(getBase64DataFromJpegFile(filename));
+
+
+const getBase64DataFromJpegFile = (filename) => readFileSync(filename).toString('binary')
+const getExifFromJpegFile = (filename) => piexif.load(getBase64DataFromJpegFile(filename))
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -44,7 +134,7 @@ function createWindow(): void {
     height: 1024,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    ...(process.platform === 'linux' ? { icon: 'icon.ico' } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
@@ -58,7 +148,7 @@ function createWindow(): void {
   mainWindow.webContents.setWindowOpenHandler((details) => {
     shell.openExternal(details.url)
     return { action: 'deny' }
-  });
+  })
 
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
@@ -67,10 +157,95 @@ function createWindow(): void {
   }
 }
 app.whenReady().then(() => {
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.electron.antiocr')
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
+
+  // trackEvent('app_started')
+
+  ipcMain.on('GATE_ACTIVATE', async (event, { key }) => {
+    const { valid, error } = await LemonActivation(key, 'activate')
+    mainStore.set('LICENSE_KEY', valid ? key : '')
+    mainStore.set('LICENSE_ACTIVE', valid)
+    const response = {
+      key: valid ? key : '',
+      errors: !valid,
+      activation: mainStore.get('ACTIVATION_STATUS'),
+      status: !valid ? 'ACTIVATION_LIMIT' : 'LICENSE_ACTIVE',
+      message: error ? error : 'License has been registered to your machine.'
+    }
+
+    // console.log( mainStore.get('LICENSE_ACTIVE') )
+    // console.log( mainStore.get('LICENSE_KEY') )
+    // console.log( mainStore.get('MACHINE_NAME') )
+    // console.log( mainStore.get('INSTANCE_ID') )
+    // console.log( mainStore.get('ACTIVATION_STATUS') )
+
+    event.sender.send('GATE_ACTIVATE_RESPONSE', response)
+  })
+  ipcMain.on('GATE_SUBMIT', async (event, { key }) => {
+    const { valid, error } = await LemonActivation(key)
+    let message = ''
+    let status = ''
+
+    if (!valid && error != null) {
+      if (error.includes('not found')) {
+        message =
+          'License key you entered could not be found. Please double-check the key and try again.'
+        status = 'NOT_FOUND'
+      } else if (error.includes('expired')) {
+        message =
+          'Sorry, the license key you entered has expired. Please renew your license to continue using the software.'
+        status = 'LICENSE_EXPIRED'
+      } else if (error.includes('disabled')) {
+        message =
+          "We're sorry, but the license key you entered has been disabled. Please contact our support team for further assistance."
+        status = 'LICENSE_DISABLED'
+      } else {
+        message = error
+        status = 'UNKNOWN_ERROR'
+      }
+    } else {
+      message =
+        'Congratulations! The license key you entered is valid and ready to activate your software. Would you like to proceed with the activation using this key?'
+      status = 'LICENSE_ACTIVE'
+    }
+
+    const response = {
+      key: valid ? key : '',
+      errors: !valid,
+      status,
+      activation: mainStore.get('ACTIVATION_STATUS'),
+      message
+    }
+    //console.log(response);
+    event.sender.send('GATE_RESPONSE', response)
+  })
+  ipcMain.on('DEACTIVATE_LICENSE', async (event) => {
+    await LemonActivation(mainStore.get('LICENSE_KEY'), 'deactivate')
+    event.sender.send('IS_LICENSED_RESPONSE', mainStore.get('LICENSE_ACTIVE'))
+  })
+  ipcMain.on('IS_LICENSED', async (event) => {
+    //mainStore.clear()
+    if (mainStore.get('LICENSE_ACTIVE')) {
+      await LemonActivation(mainStore.get('LICENSE_KEY'))
+    }
+
+    // console.log("============================================================")
+    // console.log("=============== || ACTIVATION STATUS ||=====================")
+    // console.log("============================================================")
+    // console.log("LICENSE_ACTIVE",   mainStore.get('LICENSE_ACTIVE') )
+    // console.log("LICENSE_KEY",   mainStore.get('LICENSE_KEY') )
+    // console.log("MACHINE_NAME",   mainStore.get('MACHINE_NAME') )
+    // console.log("INSTANCE_ID",   mainStore.get('INSTANCE_ID') )
+    // console.log("ACTIVATION_STATUS",   mainStore.get('ACTIVATION_STATUS') )
+    // console.log("============================================================")
+    // console.log("============================================================")
+
+    event.sender.send('IS_LICENSED_RESPONSE', mainStore.get('LICENSE_ACTIVE'))
+  })
+
   // IPC test
   ipcMain.on('open-dialog', (event) => {
     dialog
@@ -78,73 +253,76 @@ app.whenReady().then(() => {
         properties: ['openFile'],
         filters: [{ name: 'Images', extensions: ['jpg'] }]
       })
-      .then(async ({ filePaths }) => {
-        Object.assign(Store, { mime: '', image: '', raw: '', name: '', ext: '' });
-
-        if(filePaths.length > 0) {
-          const filepath = filePaths[0];
-          const newImageData = getBase64DataFromJpegFile(filepath);
-          const exif = get_random(exifs);
-          Store.mainEXIF = getExifFromJpegFile(exif);
-
+      .then(({ filePaths }) => {
+        Object.assign(Store, { mime: '', image: '', raw: '', name: '', ext: '' })
+        if (filePaths.length > 0) {
+          const filepath = filePaths[0]
+          const newImageData = getBase64DataFromJpegFile(filepath)
+          const exif = get_random(exifs)
+          Store.mainEXIF = getExifFromJpegFile(exif)
           const newExif = {
-              '0th': { ...Store.mainEXIF['0th'] },
-              'Exif': { ...Store.mainEXIF['Exif'] },
-              'GPS': { ...Store.mainEXIF['GPS'] },
-              'Interop': { ...Store.mainEXIF['Interop'] },
-              '1st': { ...Store.mainEXIF['1st'] },
-              'thumbnail': null
-          };
-
-          const newExifBinary = piexif.dump(newExif);
-          const newPhotoData = piexif.insert(newExifBinary, newImageData);
-
-          const raw = Buffer.from(newPhotoData, 'binary');
-          const newfilepath = 'exif-revised' + extname(filepath);
-          writeFileSync(newfilepath, raw);
-
-
-          // const raw = await readFile(filepath, { encoding: "utf8" });
-          const base64 = await readFile(newfilepath, { encoding: "base64" });
-
-          const objectN = {
-            mime: mime.lookup(filepath),
-            image: newfilepath,
-            raw: raw,
-            name: basename(filepath),
-            ext: extname(filepath)
+            '0th': { ...Store.mainEXIF['0th'] },
+            Exif: { ...Store.mainEXIF['Exif'] },
+            GPS: { ...Store.mainEXIF['GPS'] },
+            Interop: { ...Store.mainEXIF['Interop'] },
+            '1st': { ...Store.mainEXIF['1st'] },
+            thumbnail: null
           }
 
-          Object.assign(Store, objectN);
+          const newExifBinary = piexif.dump(newExif)
+          const newPhotoData = piexif.insert(newExifBinary, newImageData)
 
-          event.sender.send("receive-file", {
-            raw: base64,
-            name: Store.name,
-            ext: Store.ext,
-            mime: Store.mime
-          });
+          const raw = Buffer.from(newPhotoData, 'binary')
+          const newfilepath = join('./exif-revised' + extname(filepath))
+          try {
+            writeFileSync(newfilepath, raw)
+          } catch (e) {
+            throw 'Failed to save the file !' + newfilepath
+          } finally {
+            const base64 = readFileSync(newfilepath, { encoding: 'base64' })
+
+            const objectN = {
+              mime: contentType(filepath),
+              image: newfilepath,
+              raw: raw,
+              name: basename(filepath),
+              ext: extname(filepath)
+            }
+
+            Object.assign(Store, objectN)
+
+            event.sender.send('receive-file', {
+              raw: base64,
+              name: Store.name,
+              ext: Store.ext,
+              mime: Store.mime
+            })
+
+            // trackEvent('ocr_img_created')
+          }
+        } else {
+          Object.assign(Store, { mime: '', image: '', raw: '', name: '', ext: '' })
+          event.sender.send('reset')
         }
       })
-  });
-
+  })
 
   ipcMain.on('ping', (event, text) => {
-
     Jimp.read(Store.image)
       .then((image) => {
-        Jimp.loadFont("jeURtOG_ZKxNX4XOyeLeaTzn.ttf.fnt").then((font) => {
-          image.quality(100);
-          image.print(font, 10, 10, text, 500);
+        Jimp.loadFont(fontPath).then((font) => {
+          image.quality(100)
+          image.print(font, 10, 10, text, 500)
           //image.write(Store.name);
           image.getBase64Async(Store.mime).then((result) => {
-            event.sender.send('pong', result);
-          });
-        });
+            event.sender.send('pong', result)
+          })
+        })
       })
       .catch((err) => {
-        console.log(err);
-      });
-  });
+        console.log(err)
+      })
+  })
 
   createWindow()
 
--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,105 +1,210 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref, computed, onBeforeMount, reactive } from 'vue'
 import mainIcon from './assets/img/icon.png?url'
 import placeholderIMG from './assets/img/placeholder.svg?url'
 
-const ocrtext = ref('WhatsApp: +39 333 678 900');
+const ocrtext = ref('')
 //const imgexif = ref({});
-const modal = ref(false);
-const fileinput = ref('');
-const ocrtype = ref('');
-const ocrimg = ref<string | null>(null);
-const isLoading = ref(false);
+const modal = ref(false)
+const fileinput = ref('')
+const ocrtype = ref('')
+const ocrimg = ref<string | null>(null)
+const isLoading = ref(false)
 
-const disable = computed(() => ocrtext.value == '' || ocrimg.value == null);
-const isDownload = ref(false);
-const numid = computed(() => Math.floor(1000 + Math.random() * 9000));
-window.electron.ipcRenderer.on('pong', (_event, message: string) => {
-  isDownload.value = true;
-  const output: any = document.getElementById('preview');
-  const download: any = document.getElementById('download');
+const disable = computed(() => ocrtext.value == '' || ocrimg.value == null)
+const isDownload = ref(false)
 
-  download.href = message;
-  download.download = 'IMG_' + numid.value + numid.value;
-  output.src = message;
-  isLoading.value = false;
-
-});
-function DownloadIMG() {
-  const download: any = document.getElementById('download');
-  download.click();
-
-  setTimeout(() => {
-    isDownload.value = false;
-    ocrimg.value = null;
-    const output: any = document.getElementById('preview');
-    output.src = placeholderIMG;
-  }, 1000);
+function genFileName() {
+  const rand = Math.floor(1000 + Math.random() * 9000)
+  return 'IMG_' + rand * rand
 }
 
+window.electron.ipcRenderer.on('reset', () => {
+  isDownload.value = false
+  const output: any = document.getElementById('preview')
+  output.src = placeholderIMG
+  const download: any = document.getElementById('download')
+
+  ocrtype.value = ''
+  ocrimg.value = ''
+  download.href = '#'
+  download.download = ''
+  isLoading.value = false
+})
+
+window.electron.ipcRenderer.on('pong', (_event, message: string) => {
+  isDownload.value = true
+  const output: any = document.getElementById('preview')
+  const download: any = document.getElementById('download')
+
+  download.href = message
+  download.download = genFileName()
+  output.src = message
+  isLoading.value = false
+})
+function DownloadIMG() {
+  const download: any = document.getElementById('download')
+  download.click()
+
+  setTimeout(() => {
+    isDownload.value = false
+    ocrimg.value = null
+    const output: any = document.getElementById('preview')
+    output.src = placeholderIMG
+  }, 1000)
+}
 
 window.electron.ipcRenderer.on('receive-file', (_event, { raw, mime }) => {
-  const url = `data:${mime};base64,${raw}`;
+  const url = `data:${mime};base64,${raw}`
   // @ts-ignore I need it;
-  const output: any = document.getElementById('preview');
-  output.src = url;
-  isLoading.value = false;
-  ocrimg.value = url;
-  isDownload.value = false;
-});
-
-
+  const output: any = document.getElementById('preview')
+  output.src = url
+  isLoading.value = false
+  ocrimg.value = url
+  isDownload.value = false
+})
 interface dataPing {
-  text: string;
-  img: string;
-  type: string;
+  text: string
+  img: string
+  type: string
 }
 
 async function sendPing(message: dataPing) {
-  await window.electron.ipcRenderer.send('ping', message);
+  await window.electron.ipcRenderer.send('ping', message)
 }
 
-
-
 const newFile = (event) => {
-  ocrimg.value = null;
-  const files = event.target.files;
+  ocrimg.value = null
+  const files = event.target.files
   if (files.length) {
-    const File: File = files[0];
-    const objectURL = URL.createObjectURL(File);
-    ocrimg.value = objectURL;
-    ocrtype.value = File.type;
-    const output = document.getElementById('preview');
+    const File: File = files[0]
+    const objectURL = URL.createObjectURL(File)
+    ocrimg.value = objectURL
+    ocrtype.value = File.type
+    const output = document.getElementById('preview')
     // @ts-ignore I need it;
-    output.src = URL.createObjectURL(files[0]);
+    output.src = URL.createObjectURL(files[0])
     // @ts-ignore I need it;
     output.onload = function () {
       // @ts-ignore I need it;
-      URL.revokeObjectURL(output.src);
+      URL.revokeObjectURL(output.src)
     }
   }
 }
 const submitForm = () => {
-  isLoading.value = true;
+  isLoading.value = true
   const msg: dataPing = {
     text: ocrtext.value,
     // @ts-ignore I need it;
     img: ocrimg.value,
-    type: ocrtype.value,
-  };
-  sendPing(msg);
+    type: ocrtype.value
+  }
+  sendPing(msg)
 }
 
 const openDialog = async () => {
-  await window.electron.ipcRenderer.send('open-dialog');
+  await window.electron.ipcRenderer.send('open-dialog')
 }
+
+const licensed = ref(true)
+const isChecking = ref(false)
+const Lic = reactive({
+  key: '',
+  activation: false,
+  errors: false,
+  status: 'FREE_TRIAL',
+  message:
+    "Thank you for choosing our product! To unlock its full potential, please enter your license key below. If you haven't purchased one yet, you can do so on our website. Need any assistance? Feel free to reach out to our support team."
+})
+
+onBeforeMount(async () => {
+  await checkIfLicensed()
+})
+
+// ACTIVATE LICENSE ON THIS MACHINE
+async function activateLicense() {
+  isChecking.value = true
+  await window.electron.ipcRenderer.send('GATE_ACTIVATE', { key: Lic.key })
+}
+window.electron.ipcRenderer.on('GATE_ACTIVATE_RESPONSE', (_event, response) => {
+  Object.assign(Lic, { ...response })
+  console.log(Lic)
+  licensed.value = !response.errors
+  isChecking.value = false
+}) // END
+
+// CHECK IF LICENSED
+async function checkIfLicensed() {
+  await window.electron.ipcRenderer.send('IS_LICENSED')
+}
+window.electron.ipcRenderer.on('IS_LICENSED_RESPONSE', (_event, isvalid: boolean) => {
+  licensed.value = isvalid
+  if (isvalid) {
+    Lic.status = 'LICENSED'
+    Lic.message =
+      'Your software has been successfully licensed with the provided key. You now have full access to all premium features, updates, and support.'
+  } else {
+    Lic.status = 'FREE_TRIAL'
+    Lic.message =
+      "Thank you for choosing our product! To unlock its full potential, please enter your license key below. If you haven't purchased one yet, you can do so on our website. Need any assistance? Feel free to reach out to our support team."
+  }
+}) // END
+// DEACTIVATE LICENSE FUNCTION
+
+async function deactivateLicense() {
+  if (
+    confirm(
+      "Are you sure you want to deactivate your license key?\n  If you proceed, please note that you may need to re-purchase or reactivate your license in the future.\n\n  If you're sure about this decision, click 'OK' below. If you have any questions or need assistance, please contact our support team."
+    ) == true
+  ) {
+    await window.electron.ipcRenderer.send('DEACTIVATE_LICENSE')
+  }
+}
+// GATE SUBMIT FORM - CHECK LICKENSE
+async function checkLicense() {
+  isChecking.value = true
+  await window.electron.ipcRenderer.send('GATE_SUBMIT', { key: Lic.key })
+}
+window.electron.ipcRenderer.on('GATE_RESPONSE', (_event, response) => {
+  Object.assign(Lic, { ...response })
+  console.log(Lic)
+  isChecking.value = false
+}) // END
+
+// MODAL LICENSE OPEN
+function openModal(action: boolean) {
+  if (action) {
+    checkIfLicensed()
+  }
+  modal.value = action
+} // END
 </script>
 
 <template>
+  <teleport to="#clear" v-if="licensed">
+    <div>
+      <small style="margin-bottom: 5px; display: block; color: red">
+        <a href="#" @click.prevent="deactivateLicense">Deactivate application</a>
+      </small>
+    </div>
+  </teleport>
+
+  <teleport to="#license" v-else>
+    <div style="background-color: #fff; text-align: center">
+      <small style="margin-bottom: 5px; display: block; color: red">
+        <span>Unlicensed! Free 30 day trial.</span> |
+        <span>Click</span>
+        <span style="margin: 0 5px"
+          ><a style="color: black" href="#" @click.prevent="openModal(true)">here</a></span
+        >
+        <span>to enter license key. </span>
+      </small>
+    </div>
+  </teleport>
   <img :src="mainIcon" alt="Icon" />
   <form @submit.prevent="submitForm">
     <label for="title">Anti OCR Text</label>
-    <div style="text-align: center;">
+    <div style="text-align: center">
       <a href="#" @click.prevent="openDialog">
         <span v-if="ocrimg != null">Change Image</span>
         <span v-else>Select Image</span>
@@ -108,7 +213,8 @@ const openDialog = async () => {
     <input ref="fileinput" class="file" type="file" accept="image/*" @change="newFile" />
     <input
       v-model="ocrtext"
-      :aria-busy="isLoading" aria-label="Please wait…"
+      :aria-busy="isLoading"
+      aria-label="Please wait…"
       class="ocrtext"
       name="ocrtext"
       type="text"
@@ -134,34 +240,107 @@ const openDialog = async () => {
     </button>
 
     <hr />
-    <div class="output" style="text-align: center;">
+    <div class="output" style="text-align: center">
       <img
         id="preview"
         :src="placeholderIMG"
         alt="No image"
-        style="width: 350px; height: auto; object-fit: cover; aspect-ratio: 1/1; border-radius: 20px;"
+        style="
+          width: 350px;
+          height: auto;
+          object-fit: cover;
+          aspect-ratio: 1/1;
+          border-radius: 20px;
+        "
       />
-      <a href="#" id="download" style="margin: 0 auto; visibility: hidden;" download="file.jpg">download image</a>
+      <a href="#" id="download" style="margin: 0 auto; visibility: hidden" download="file.jpg"
+        >download image</a
+      >
     </div>
   </form>
   <!-- <Versions /> -->
-  <dialog :open="modal">
+  <dialog :open="modal" class="activation-modal">
     <article>
       <header>
-        <button aria-label="Close" rel="prev"></button>
+        <button aria-label="Close" rel="prev" @click.prevent="openModal(false)"></button>
         <p>
-          <strong>🗓️ Thank You for Registering!</strong>
+          <strong>🗓️ Application licensing</strong>
         </p>
       </header>
-      <p>
-        We're excited to have you join us for our
-        upcoming event. Please arrive at the museum
-        on time to check in and get started.
-      </p>
-      <ul>
-        <li>Date: Saturday, April 15</li>
-        <li>Time: 10:00am - 12:00pm</li>
-      </ul>
+      <div
+        :class="{ errors: Lic.errors, success: licensed && Lic.status == 'LICENSE_ACTIVE' }"
+        class="lic"
+      >
+        <p>{{ Lic.message }}</p>
+        <hr style="max-width: 60%" />
+        <div class="status">
+          <span>
+            <span>Status:</span> <strong>{{ Lic.status }}</strong>
+          </span>
+        </div>
+      </div>
+      <div>
+        <form v-if="!licensed && Lic.status == 'LICENSE_ACTIVE'" @submit.prevent="activateLicense">
+          <div>
+            <input
+              v-model="Lic.key"
+              required
+              :disabled="isChecking"
+              type="text"
+              placeholder="8c339b42-8355****************"
+              :aria-invalid="Lic.errors"
+            />
+          </div>
+          <div>
+            <button
+              style="max-width: 350px"
+              :disabled="isChecking"
+              type="submit"
+              class="dlbutton"
+              :aria-busy="isChecking"
+              aria-label="Please wait…"
+            >
+              <span>ACTIVATE</span>
+            </button>
+          </div>
+        </form>
+        <form v-else-if="licensed && Lic.activation">
+          <div>
+            <input
+              v-model="Lic.key"
+              required
+              :disabled="true"
+              type="text"
+              placeholder="8c339b42-8355****************"
+              :aria-invalid="Lic.errors"
+            />
+          </div>
+        </form>
+        <form v-else @submit.prevent="checkLicense">
+          <div>
+            <input
+              v-model="Lic.key"
+              required
+              :disabled="isChecking"
+              type="text"
+              placeholder="8c339b42-8355****************"
+              :aria-invalid="Lic.errors"
+            />
+          </div>
+          <div>
+            <button
+              style="max-width: 350px"
+              :disabled="isChecking"
+              type="submit"
+              class="dlbutton"
+              :aria-busy="isChecking"
+              aria-label="Please wait…"
+            >
+              <span>Submit License</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </article>
   </dialog>
 </template>
